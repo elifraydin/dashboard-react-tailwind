4c1e97f63fc4e0abebe6c1bdafd0a928bcc68880
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-25 18:26:40 +0300

    Add Recoil

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,31 +1,38 @@
-import React from 'react'
 import { navLinks } from './utils/NavDB'
+import { useRecoilState } from "recoil";
+import { ActiveTabState } from "../../atoms/ActiveTabState";
 
 function NavBar() {
     return (
         <nav className='col-span-2 border-r border-gray-200 min-h-[90vh] w-[80px] xl:w-[250px] pt-8 px-1 flex flex-col items-start justify-between'>
             <div className='space-y-8 w-full'>
                 {navLinks.slice(0,4).map((link) => (
-                    <div key={link.id} 
-                    className="w-full flex items-center justify-start space-x-8 px-5 cursor-pointer group hover:border-gray-900 border-l-4 border-transparent">
-                        <span>{link.icon}</span>
-                        <h1 className='text-gray-600 group-hover:text-black xl:flex hidden'>
-                            {link.title}
-                        </h1>
-                    </div>
+                    <NavItem link={link} key={link.id} />
                 ))}
                 <div className='w-full border-t border-gray-200'></div>
                 {navLinks.slice(4,6).map((link) => (
-                    <div key={link.id} 
-                    className="w-full flex items-center justify-start space-x-8 px-5 cursor-pointer group hover:border-gray-900 border-l-4 border-transparent">
-                        <span>{link.icon}</span>
-                        <h1 className='text-gray-600 group-hover:text-black xl:flex hidden'>
-                            {link.title}
-                        </h1>
-                    </div>
+                    <NavItem link={link} key={link.id} />
                 ))}
             </div>
         </nav>
+    )
+}
+
+function NavItem({link}){
+    const [activeNavItem, setActiveNavItem] = useRecoilState(ActiveTabState);
+    return(
+        <div
+        onClick={()=>setActiveNavItem(link.id)}
+        className={`w-full flex items-center justify-start space-x-8 px-5 cursor-pointer group hover:border-gray-900 border-l-4 border-transparent ${activeNavItem === link.id && "border-gray-900 "} `}
+        
+        >
+            <span>{link.icon}</span>
+            <h1 className={`text-gray-600 group-hover:text-black xl:flex hidden
+            ${activeNavItem === link.id && "text-black "}
+            `}>
+                {link.title}
+            </h1>
+        </div>
     )
 }
 export default NavBar
